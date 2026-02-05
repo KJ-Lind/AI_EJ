94359915438135bfb95d5a5b0acf11dd1b56ac21
KJ-Lind <lind@esat-alumni.com>
2026-02-05 16:22:11 +0100

    Fixing Issue in Markov Chains

--- Markov_Chains.py
+++ Markov_Chains.py
@@ -3,7 +3,6 @@ import random
 
 # Window
 Window = tk.Tk()
-Window.configure(bg="grey")
 Window.title("Cadenas Markov")
 Window.geometry("900x800")
 
@@ -13,10 +12,10 @@ a_chance = tk.DoubleVar(value=0.5)
 b_chance = tk.DoubleVar(value=0.5)
 
 # UI
-title = tk.Label(Window, text="Markov chain simulator", font=("Arial", 20), bg="grey")
+title = tk.Label(Window, text="Markov chain simulator", font=("Arial", 20))
 title.pack(pady=50)
 
-label_A = tk.Label(Window, text="Chances of going to A",bg="grey")
+label_A = tk.Label(Window, text="Chances of going to A")
 label_A.pack()
 
 slider_A = tk.Scale(
@@ -26,12 +25,11 @@ slider_A = tk.Scale(
     resolution=0.01,
     orient="horizontal",
     length=400,
-    variable=a_chance,
-    bg="grey"
+    variable=a_chance
 )
 slider_A.pack()
 
-label_B = tk.Label(Window, text="Chances of going to B", bg="grey")
+label_B = tk.Label(Window, text="Chances of going to B")
 label_B.pack()
 
 slider_B = tk.Scale(
@@ -42,15 +40,13 @@ slider_B = tk.Scale(
     orient="horizontal",
     length=400,
     variable=b_chance,
-    bg="grey"
 )
 slider_B.pack()
 
 label_state = tk.Label(
     Window,
     text=f"Current state: {current_state}",
-    font=("Arial", 18),
-    bg="grey"
+    font=("Arial", 18)
 )
 label_state.pack(pady=40)
 
